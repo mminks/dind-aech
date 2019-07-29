4c7d6edc833559f3106f2dca9fc2a8fb9f088d31
Michael Kunze <mkunze@inoxio.de>
2019-07-29 11:40:33 +0200

    Remove ssh config initialization
    This is done in the dockerfile.

--- bin/setup_ssh.sh
+++ bin/setup_ssh.sh
@@ -4,9 +4,5 @@ set -eo pipefail
 
 AWS_KEYFILE=${1?"Missing AWS s3 path to private key"}
 
-# Setup ssh
-mkdir ~/.ssh
-echo -e "Host *\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile /dev/null\n\tLogLevel ERROR" > ~/.ssh/config
-
 # initialize the ssh agent with the key
 aws s3 cp s3://${AWS_KEYFILE} - | ssh-add -
