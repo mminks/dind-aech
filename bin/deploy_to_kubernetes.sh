4d0bbd33844ea3b734003006227e30e66ddef023
Meik Minks <mminks@inoxio.de>
2020-06-03 23:12:59 +0200

    I will not delete necessary code anymore...

--- bin/deploy_to_kubernetes.sh
+++ bin/deploy_to_kubernetes.sh
@@ -6,11 +6,15 @@ MANIFEST=${1?"Please specify the manifest files or directory you want to deploy 
 DEPLOYMENT_NAME=${2}
 NAMESPACE=${NAMESPACE:-default}
 
+if [[ -d "${MANIFEST}" ]]; then
+  MANIFEST="${MANIFEST}/*"
+fi
+
 echo "Starting deployment..."
 
 cat ${MANIFEST} | envsubst | kubectl apply -f -
 
-if [ -n "${DEPLOYMENT_NAME}" ]; then
+if [[ -n "${DEPLOYMENT_NAME}" ]]; then
   echo "Watching deployment status..."
 
   kubectl -n ${NAMESPACE} rollout status "${DEPLOYMENT_NAME}"
