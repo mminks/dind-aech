9639f135ef7fa7e5b0cfd0e5414994f3316e61d4
Michael Kunze <mkunze@inoxio.de>
2020-06-26 12:26:12 +0200

    Add aws env to kubernetes deployment

--- bin/deploy_to_kubernetes.sh
+++ bin/deploy_to_kubernetes.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env sh
 
-set -eo pipefail
+set -e
 
 MANIFEST=${1?"Please specify the manifest files or directory you want to deploy to kubernetes"}
 DEPLOYMENT_NAME=${2}
-NAMESPACE=${NAMESPACE:-default}
+ENV_PATH=${3}
 
-if [[ -d "${MANIFEST}" ]]; then
+if [ -d "${MANIFEST}" ]; then
   MANIFEST="${MANIFEST}/*"
 fi
+
+# get secrets from SSM if we are suppose to
+[ -n "${ENV_PATH}" ] && eval "$(AWS_REGION=${AWS_DEFAULT_REGION} AWS_ENV_PATH=${ENV_PATH} /usr/local/bin/aws-env)"
 
 echo "Starting deployment..."
 
 cat ${MANIFEST} | envsubst | kubectl apply -f -
 
-if [[ -n "${DEPLOYMENT_NAME}" ]]; then
+if [ -n "${DEPLOYMENT_NAME}" ]; then
   echo "Watching deployment status..."
 
-  kubectl -n ${NAMESPACE} rollout status "${DEPLOYMENT_NAME}"
+  kubectl -n "${NAMESPACE:-default}" rollout status "${DEPLOYMENT_NAME}"
 fi
